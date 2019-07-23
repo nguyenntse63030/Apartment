29ca994fef93b0c4cfae7b26d598ab8018bc915c
NguyenNT3498 <nguyentrungnguyen3498@gmail.com>
2019-07-23 18:55:32 +0700

    Chỉnh lại swagger cho Lễ.

--- api/cronjobController.js
+++ api/cronjobController.js
@@ -2,8 +2,6 @@ var cron = require('node-cron')
 const userController = require('./v1/controllers/userController')
 const billController = require('./v1/controllers/billController')
 
-
-
 const RUN_AT_1_AM = '0 0 1 * * *'
 const RUN_AT_6_AM_30 = '0 30 6 * * *'
 const RUN_AT_BEGIN_OF_MONTH = '1 0 0 1 * *'
