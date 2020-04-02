3d9774d0e55241a72673271364b75c5cd6e3084e
nguyenntse63030 <41613848+nguyenntse63030@users.noreply.github.com>
2020-04-02 14:30:43 +0700

    Edit config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  uriMongo: 'mongodb://swd391:swd391@ds263816.mlab.com:63816/apartment',
-  tokenExpire: 3 * 12 * 30 * 24 * 60 * 60,
-  secret: 'SWD391-1998',
-  uploadKey: 'c940089d6e1e3588e2db9c277d519696'
+    uriMongo: 'mongodb://swd391:swd391@ds263816.mlab.com:63816/apartment',
+    tokenExpire: 3 * 12 * 30 * 24 * 60 * 60,
+    secret: 'SWD391-1998',
+    AWS: {
+        ACCESS_KEY_ID: 'AKIAJUQVQAPCOPSFBXJQ',
+        SECRETT_KEY: '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',
+        BUCKET_NAME: 'prc391-bucket'
+    }
 }
